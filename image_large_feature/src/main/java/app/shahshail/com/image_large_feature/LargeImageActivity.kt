53e651ac7fd875df14dfa4513d1457abd212e5d8
Shail Shah <shahshail9898@gmail.com>
2019-01-29 20:24:14 -0600

    Module : Image large feature

---
+++ image_large_feature/src/main/java/app/shahshail/com/image_large_feature/LargeImageActivity.kt
@@ -0,0 +1,12 @@
+package app.shahshail.com.image_large_feature
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class LargeImageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_large_image)
+    }
+}
